47b2f023ab586e863c066b88abf0123a4db16a5f
krystalmm <taeyeon_aina@live.jp>
2021-02-04 11:42:28 +0000

    [add] users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  
   def show
+    @user = User.find(params[:id])
+    # @userに関連付けられた投稿（.post_images）のみ@post_imagesに渡される！！
+    @post_images = @user.post_images.page(params[:page]).reverse_order
   end
+
 end
